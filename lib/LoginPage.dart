4f9fad7873789ec638d935ddd1e6de891c28e6f1
Walter Corrales <corraleswalter@live.com>
2020-03-06 14:44:51 -0600

    Login page animation cooool 😁

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:animator/animator.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -6,8 +9,47 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
   bool displayed = true;
+  AnimationController animationController;
+  AnimationController animationController2;
+  Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    // animationController = new AnimationController(
+    //   duration: new Duration(seconds: 30),
+    //   vsync: this,
+    // );
+    // animationController.repeat();
+
+    // _animation = new Tween<double>(
+    //   begin: 0,
+    //   end: 5,
+    // ).animate(animationController)
+    //   ..addStatusListener((status) {
+    //     if (status == AnimationStatus.completed) {
+    //       animationController.reverse();
+    //     } else if (status == AnimationStatus.dismissed) {
+    //       animationController.forward();
+    //     }
+    //   });
+    // _animation.
+
+    // animationController2 = new AnimationController(
+    //   duration: new Duration(milliseconds: 500),
+    //   vsync: this,
+    // );
+    // animationController2.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +60,65 @@ class _LoginPageState extends State<LoginPage> {
           children: <Widget>[
             Container(
               height: MediaQuery.of(context).size.height,
-              color: Color(0xff4B8563),
+              color: Color(0xff2DBEEA),
+            ),
+            Positioned(
+              top: 10,
+              child: Animator(
+                repeats: 0,
+                duration: Duration(seconds: 30),
+                tween: Tween<double>(begin: 0, end: 2 * pi),
+                builder: (anim) => Transform.rotate(
+                  angle: anim.value,
+                  child: Container(
+                    height: 350,
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset('resources/tierra.png'),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 100,
+              left: 50,
+              child: Animator(
+                // repeats: 0, // REPEAT FROM START
+                cycles: 0, // REVERSE ANIMATION
+                duration: Duration(milliseconds: 500),
+                resetAnimationOnRebuild: false,
+                tween: Tween<Offset>(begin: Offset(0, 0), end: Offset(2, 5)),
+                builder: (anim) => Transform.translate(
+                  offset: anim.value,
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    child: Image.asset('resources/nube1.png'),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 200,
+              right: 50,
+              child: Animator(
+                // repeats: 0, // REPEAT FROM START
+                cycles: 0, // REVERSE ANIMATION
+                duration: Duration(milliseconds: 500),
+                resetAnimationOnRebuild: false,
+                tween: Tween<Offset>(begin: Offset(0, 0), end: Offset(2, 5)),
+                builder: (anim) => Transform.translate(
+                  offset: anim.value,
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    child: Image.asset('resources/nube2.png'),
+                  ),
+                ),
+              ),
             ),
             AnimatedContainer(
               duration: Duration(milliseconds: 250),
-              height: displayed
-                  ? MediaQuery.of(context).size.height - 60
-                  : MediaQuery.of(context).size.height / 2,
+              height: displayed ? MediaQuery.of(context).size.height - 60 : 0,
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
                 color: Color(0xff334339),
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,31 @@
-import 'package:ecoapp/LoginPage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ecoapp/LoginPage.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      appBarTheme: AppBarTheme(
+        brightness: Brightness.light,
+        elevation: 0,
+        color: ThemeData.light().canvasColor,
+        textTheme: TextTheme(
+          title: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      home: LoginPage(),
-    );
-  }
+      primarySwatch: Colors.green,
+    ),
+    home: LoginPage(),
+  ));
 }
